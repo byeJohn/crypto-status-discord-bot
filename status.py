6f52bd7d75ba33063f6469956b92f253fc31f297
John Nguyen <75657889+byeJohn@users.noreply.github.com>
2023-02-04 16:33:06 -0500

    Update status.py

--- status.py
+++ status.py
@@ -1,15 +1,30 @@
 import discord
 import requests
 import asyncio
+from discord.ext import commands
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix='?', intents=intents)
+intents.message_content = True
 
 TOKEN = ''
 
+# sets the amount of minutes per check
 interval = 1
 
+# coingecko link of the desired coin. 
 coin = ''
 
+@client.event
+  async def on_ready():
+    print("ONLINE!!!")
+    response = requests.get(coin)
+    data = response.json()
+    
+    
+@client.command()
+async def servers(ctx):
+    await ctx.send(f"Servers: {len(client.guilds)}")
 
 
 
