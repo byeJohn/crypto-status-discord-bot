dc083d139b50023f447c8eb39fdeb867ab871c65
John Nguyen <75657889+byeJohn@users.noreply.github.com>
2023-02-04 03:06:07 -0500

    Create status.py

---
+++ status.py
@@ -0,0 +1,17 @@
+import discord
+import requests
+import asyncio
+
+client = discord.Client(intents=discord.Intents.default())
+
+TOKEN = ''
+
+interval = 1
+
+coin = ''
+
+
+
+
+
+client.run(TOKEN)
